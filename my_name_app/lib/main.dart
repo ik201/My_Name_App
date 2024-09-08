554b2d47014bae1b73019454c23d7933e10e3810
Ikem Samuel <ikemcharlseanthony@gmail.com>
2024-09-08 07:38:36 -0700

    first commit

---
+++ my_name_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp (
+
+     MaterialApp( 
+      home: Scaffold(   
+        appBar: AppBar(  
+          title: const Text('My Name App'),
+        ),
+        body: const Center(  
+         child: Text('CHARLSE'),
+          ),
+         ),
+        ),  
+      );
+}
